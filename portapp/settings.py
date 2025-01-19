4eb9c795248892693ee40246d32ae0dd605b8ba0
saadWindows <saadiqbal1921@gmail.com>
2025-01-19 22:54:14 +0530

    v1.8 without creds

--- portapp/settings.py
+++ portapp/settings.py
@@ -140,4 +140,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'saadiqbal1921@gmail.com'
-EMAIL_HOST_PASSWORD = 'oightneassiouder'
+EMAIL_HOST_PASSWORD = 'test'
